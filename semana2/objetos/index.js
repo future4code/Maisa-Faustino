fde3e29bfa626c944ca0b06e2fd6093dcde19e63
08-06Maisa <may.b.faustino@hotmail.com>
2021-06-21 16:23:19 -0300

    Exercicios incompletos da semana2-aula07

---
+++ semana2/objetos/index.js
@@ -0,0 +1,28 @@
+// ---------------Exercicio de interpretação de código-------------
+// 1- console.log (filme.elenco [0]) // irá imprimir Matheus Nachtergaele 
+//    console.log (filme.elenco [filme.elenco.lenght - 1]) // irá imprimir o último elemento que será Virginia Canvendish
+//    console.log(filme.transmissoesHoje[2]) // irá imprimir canal: "Globo" horario: "19h"
+// 
+// 2- Irá imprimir todo o objeto cachorro. Irá imprimir o objeto gato copiando a informação do objeto cachorro alterando  o nome. Irá imprimir o objeto tartaruga, copiando as informações do objeto gato trocando a letra a por o. A sintaxe dos ... significa que irá copiar o objeto podendo modificar o valor de algum elemento.
+// 3- 
+// ----------------Exercicio de escrita de código------------------
+// 1-
+
+// const pessoa = {
+//     nome: 'Arthur',
+//     apelido: [ 'Tutu' , 'Bubu' , 'Nenenzinho' ]
+// }
+
+// console.log(`Eu sou ${pessoa.nome} mas pode me chamar de ${pessoa.apelido}`)
+
+
+const pessoa1 = {
+    nome: 'Marcos' ,
+    idade: 29 ,
+    profissao: 'serralheiro'
+}
+ const pessoa2 = {
+     nome: 'Geraldo',
+     idade: 80 , 
+     profissao: 'aposentado'
+ }
