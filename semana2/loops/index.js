5d71417dfefbdf8f980b18301f75374d1fb69823
08-06Maisa <may.b.faustino@hotmail.com>
2021-06-28 14:44:10 -0300

    Fiz o exercicio 1

--- semana2/loops/index.js
+++ semana2/loops/index.js
@@ -1,8 +1,8 @@
 // ----------------Exercicio de interpretação de código----------
 // 
-// 1- a variavel valor tem o valor de 0, depois vai declarar um laço for que para o valor de i for igual a 0 e menor que 5 ele vai ter oincremento de i++ para percorrer o código de um em um até chegar no valor de 5, em que irá sair do laço e imprimir no console o valor de i.
+// 1- a variavel valor tem o valor de 0, depois vai declarar um laço for com a variavel i = 0 e i menor que 5, e a variavel valor será valor + 1. Ou seja vai rodar o loop 5 vezes e depois vai imprimir o a variavel valor que será 10. Pois 0 + 0 = 0, 0 + 1 = 1, 1 + 2 = 3, 3 + 3 = 6 , 6 + 4 = 10, então a variavel valor será valor + i =  6 + 4 = 10. 
 // 
-// 2- a) tem uma variavel é é um array, então o loop for irá avaliar cada numero da variavel lista e o que for maior de 18 irá ser imprimido no console.
+// 2- a) tem uma variavel é um array, então o loop for irá avaliar cada numero da variavel lista e o que for maior de 18 irá ser imprimido no console. 19, 21, 23, 25, 27, 30
 //    b) o for..of é indicado para ler os elementos do array diretamente, sem passar pelo indice, no caso se quiser acessar o elemento do array pelo indice é indicado usar o for.
 // 
 // 3- Enquanto a variavel quantidadeAtual for menor que a variavel quantidadeTotal vai imprimir uma linha e para a variavel asterisco o valor é igual a 0 e menor que a variavel quantidadeTotal + 1 ela vai incrementar de 1 em 1 a linha + asterico até chegar na quantidade que o usuário inseriu.
@@ -11,12 +11,18 @@
 // 
 // 1-
 
-const quantidadeDePet = number(prompt('Quantos pets você tem?'))
+// const quantidadeDePet = Number(prompt('Quantos pets você tem?'))
+// if (quantidadeDePet === 0) {
+//     console.log('Que pena! Você pode adotar um pet!')
+// }
 
-if (quantidadeDePet == 0) {
-    console.log('Que pena! Você pode adotar um pet!')
-}
+// if (quantidadeDePet > 0) {
+//     let arrayDePet = []
+//     for( let i = 0 ; i < quantidadeDePet; i++){
+//         arrayDePet.push(prompt('Diga o nome do seu pet'))
+//     }
+//     console.log (arrayDePet)
+// }
+// 2-
 
-while (quantidadeDePet > 0) {
-    const nomePet = prompt('Qual é o nome do seu pet?')
-}
+const arrayOriginal = []
---
+++ semana3/revisao-js/exercicios.js
@@ -0,0 +1,141 @@
+// EXERCÍCIO 01
+// function inverteArray(array) {
+//   let novoArray = [];
+
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     novoArray.push(array[i]);
+// }
+//   return novoArray;
+// }
+
+// EXERCÍCIO 02
+function retornaNumerosParesElevadosADois(array) {
+  let novoArray = [1, 2, 3, 4, 5, 6]
+  for (let i = 1; i < array.lenght; i++) {
+    if (i % 2 === 0) {
+      novoArray.push(i)
+    }
+  }
+  return novoArray
+}
+// EXERCÍCIO 03
+// function retornaNumerosPares(array) {
+
+// }
+
+// EXERCÍCIO 04
+// function retornaMaiorNumero(array) {
+
+// }
+
+// EXERCÍCIO 05
+// function retornaQuantidadeElementos(array) {
+
+// }
+
+// EXERCÍCIO 06
+// function retornaExpressoesBooleanas() {
+
+// }
+
+// EXERCÍCIO 07
+// function retornaNNumerosPares(n) {
+
+// }
+
+// EXERCÍCIO 08
+// function checaTriangulo(a, b, c) {
+  // return 'Escaleno'
+  // return 'Equilátero'
+  // return 'Isósceles'
+// }
+
+// EXERCÍCIO 09
+// function comparaDoisNumeros(num1, num2) {
+  // Formato do objeto a ser retornado:
+  // {
+  //   maiorNumero: X,
+  //   maiorDivisivelPorMenor: Y,
+  //   diferenca: Z
+  // }
+// }
+
+// EXERCÍCIO 10
+// function segundoMaiorEMenor(array) {
+
+// }
+
+// EXERCÍCIO 11
+// function ordenaArray(array) {
+
+// }
+
+// EXERCÍCIO 12
+// function filmeFavorito() {
+
+// }
+
+// EXERCÍCIO 13
+// function imprimeChamada() {
+//   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+// }
+
+// EXERCÍCIO 14
+// function criaRetangulo(lado1, lado2) {
+
+// }
+
+// EXERCÍCIO 15
+// function anonimizaPessoa(pessoa) {
+
+// }
+
+// EXERCÍCIO 16A
+// function maioresDe18(arrayDePessoas) {
+
+// }
+
+// EXERCÍCIO 16B
+// function menoresDe18(arrayDePessoas) {
+
+// }
+
+// EXERCÍCIO 17A
+// function multiplicaArrayPor2(array) {
+
+// }
+
+// EXERCÍCIO 17B
+// function multiplicaArrayPor2S(array) {
+
+// }
+
+// EXERCÍCIO 17C
+// function verificaParidade(array) {
+
+// }
+
+// EXERCÍCIO 18A
+// function retornaPessoasAutorizadas(pessoas) {
+
+// }
+
+// EXERCÍCIO 18B
+// function retornaPessoasNaoAutorizadas(pessoas) {
+
+// }
+
+// EXERCÍCIO 19A
+// function ordenaPorNome(consultasNome) {
+
+// }
+
+// EXERCÍCIO 19B
+// function ordenaPorData(consultasData) {
+
+// }
+
+// EXERCÍCIO 20
+// function calculaSaldo(contas) {
+
+// }
