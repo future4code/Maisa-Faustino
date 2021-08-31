cb33c27f7e47400d80b0acf8595deb1446015286
08-06Maisa <may.b.faustino@hotmail.com>
2021-09-01 00:06:32 +0100

    Iniciei o projeto cookenu e pausei na parte do header

---
+++ semana12/cookenu/cookenu/src/pages/RecipeListPage/RecipeListPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+
+const RecipeListPage = () => {
+    return (
+        <div>
+            <h1>RecipeListPage </h1>
+            <Button variant="contained" color="theme.pallete.primary.main">
+                Primary
+            </Button>
+        </div>
+    )
+}
+
+export default RecipeListPage 
