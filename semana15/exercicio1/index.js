5c752a6480b18bf9fba6da644ecfe9def77c4c49
08-06Maisa <may.b.faustino@hotmail.com>
2021-09-21 00:41:41 +0100

    Fiz o o primeiro exercicio

---
+++ semana15/exercicio1/index.js
@@ -0,0 +1,10 @@
+// Acessamos os parâmetros com o process.argv[0] e process.argv[1] que são respectivamente o node e o nome-do-arquivo.
+
+
+// 1-
+const nome = "Maisa"
+const idade = Number(25)
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7} `)
+
+// 2-
+
